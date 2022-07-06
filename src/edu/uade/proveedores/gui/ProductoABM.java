259d30e23ce69dde9621bcaa499155ff1814850f
gnnavarte <gnavarte@uade.edu.ar>
2022-07-06 13:24:08 -0300

    Validacion de "precio por unidad". Solo numericos

--- src/edu/uade/proveedores/gui/ProductoABM.java
+++ src/edu/uade/proveedores/gui/ProductoABM.java
@@ -8,10 +8,7 @@ import edu.uade.proveedores.enumeration.TipoRubro;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import java.awt.event.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -99,6 +96,16 @@ public class ProductoABM extends JDialog {
 
         lblPrecioPorUnidad = new JLabel("Precio Por Unidad");
         txtPrecioPorUnidad = new JFormattedTextField();
+        txtPrecioPorUnidad.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+                char c = e.getKeyChar();
+                if (!Character.isDigit(c) && c != '.') {
+                    e.consume();
+                }
+            }
+        });
         txtPrecioPorUnidad.setColumns(10);
 
         lblDetallePorUnidad = new JLabel("Detalle por unidad");
